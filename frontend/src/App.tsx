897f1536b15730ced419652d7520309708d50518
Manu04Hubb <emanuelodhiambo84@gmail.com>
2026-05-29 08:07:24 +0300

    my first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+
+import './index.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="flex min-h-screen items-center justify-center bg-slate-900">
+      <h1 className="text-4xl font-bold text-sky-400 underline">
+        React + TS + Tailwind Works!
+      </h1>
+    </div>
+    </>
+  )
+}
+
+export default App
